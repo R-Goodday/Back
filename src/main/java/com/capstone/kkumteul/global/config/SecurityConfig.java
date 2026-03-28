4a7fe2972cb99bc5382d39e35e646d058e571731
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-03-28 22:56:50 +0900

    Feat: auth 관련 접근에 대해서 모두 인증 불필요로 선언

--- src/main/java/com/capstone/kkumteul/global/config/SecurityConfig.java
+++ src/main/java/com/capstone/kkumteul/global/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
 
                 // API path 마다 역할 기반 인가 정책 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/*").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()   // 필터링되지 않은 모든 URL에 대해서 인증 강제
                 )
 
