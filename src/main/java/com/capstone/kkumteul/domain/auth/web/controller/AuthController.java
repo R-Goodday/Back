2caff05598e96418183e1f26d8f1e9f6d875d116
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-03-28 22:48:32 +0900

    Feat: 회원가입 요청에 대한 컨트롤러 진입점 작성, Authorization 필드에 Bearer 토큰 삽입 후 반환

---
+++ src/main/java/com/capstone/kkumteul/domain/auth/web/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.capstone.kkumteul.domain.auth.web.controller;
+
+import com.capstone.kkumteul.domain.auth.service.AuthService;
+import com.capstone.kkumteul.domain.auth.web.dto.SignUpReq;
+import com.capstone.kkumteul.global.response.SuccessResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<SuccessResponse<?>> signUp(
+            // 회원가입 요청 DTO
+            @Valid @RequestBody SignUpReq req,
+            // Authorization 필드 접근용 응답 객체
+            HttpServletResponse response
+    ) {
+
+        // 회원가입 비즈니스 메소드 호출
+        String result = authService.saveUser(req);
+
+        // Authorization 필드에 Bearer 토큰 삽입
+        response.addHeader("Authorization", "Bearer " + result);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(SuccessResponse.empty());
+    }
+}
