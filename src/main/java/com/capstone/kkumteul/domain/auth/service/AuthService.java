5c2d574ef640fca4b751ce5008bb2d638878c6b6
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-03-28 22:39:40 +0900

    Fix: createUser() 메소드의 반환 타입을 Void -> String으로 수정(토큰 반환용)

--- src/main/java/com/capstone/kkumteul/domain/auth/service/AuthService.java
+++ src/main/java/com/capstone/kkumteul/domain/auth/service/AuthService.java
@@ -5,5 +5,5 @@ import com.capstone.kkumteul.domain.auth.web.dto.SignUpReq;
 public interface AuthService {
 
     // 회원가입 시 유저를 DB에 저장하는 메소드
-    Void saveUser(SignUpReq req);
+    String saveUser(SignUpReq req);
 }
