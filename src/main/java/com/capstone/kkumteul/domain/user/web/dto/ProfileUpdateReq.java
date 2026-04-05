d2506d52432b5291a8643767a28ab50076d40948
zzuhannn <lunaaeric41@naver.com>
2026-04-05 18:12:05 +0900

    feat: age 필드에 범위 검증 추가 (@Min 1, @Max 10)

--- src/main/java/com/capstone/kkumteul/domain/user/web/dto/ProfileUpdateReq.java
+++ src/main/java/com/capstone/kkumteul/domain/user/web/dto/ProfileUpdateReq.java
@@ -1,6 +1,8 @@
 package com.capstone.kkumteul.domain.user.web.dto;
 
 import com.capstone.kkumteul.domain.user.entity.Gender;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -11,5 +13,7 @@ public class ProfileUpdateReq {
     private Gender gender;
 
     @NotNull(message = "age는 필수입니다")
+    @Min(value = 1, message = "나이는 1 이상이어야 합니다")
+    @Max(value = 10, message = "나이는 10 이하여야 합니다")
     private Integer age;
 }
