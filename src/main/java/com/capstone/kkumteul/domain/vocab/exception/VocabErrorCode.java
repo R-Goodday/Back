da8b57502d592a193fe299578f134732df482435
zzuhannn <lunaaeric41@naver.com>
2026-04-30 22:54:48 +0900

    feat: VocabErrorCode 및 예외 클래스 추가
    - VocabErrorCode enum (PARAGRAPH_NOT_FOUND_FOR_VOCAB, VOCAB_FORBIDDEN, VOCAB_EXTRACT_FAILED)
    - ParagraphNotFoundForVocabException
    - VocabForbiddenException

---
+++ src/main/java/com/capstone/kkumteul/domain/vocab/exception/VocabErrorCode.java
@@ -0,0 +1,20 @@
+package com.capstone.kkumteul.domain.vocab.exception;
+
+import com.capstone.kkumteul.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.capstone.kkumteul.global.constant.StaticValue.*;
+
+@Getter
+@AllArgsConstructor
+public enum VocabErrorCode implements BaseResponseCode {
+
+    PARAGRAPH_NOT_FOUND_FOR_VOCAB("VOCAB_404_1", NOT_FOUND, "해당 페이지의 본문을 찾을 수 없습니다."),
+    VOCAB_FORBIDDEN("VOCAB_403_1", FORBIDDEN, "본인 동화의 단어장만 조회할 수 있습니다."),
+    VOCAB_EXTRACT_FAILED("VOCAB_500_1", INTERNAL_SERVER_ERROR, "단어장 추출에 실패했습니다.");
+
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+}
