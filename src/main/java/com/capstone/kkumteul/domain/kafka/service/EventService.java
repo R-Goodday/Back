1fcde037857995ed945ac5992e6bb0f16adba001
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-04 14:54:22 +0900

    Feat: Kafka에 메세지를 등록하도록 하는 서비스 객체와 동화 생성 이벤트를 전파하도록 하는 메소드 작성

---
+++ src/main/java/com/capstone/kkumteul/domain/kafka/service/EventService.java
@@ -0,0 +1,33 @@
+package com.capstone.kkumteul.domain.kafka.service;
+
+import com.capstone.kkumteul.domain.fairytale.web.dto.FairytaleGenerateReq;
+import com.capstone.kkumteul.domain.kafka.dto.FairytaleGenerateMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/* 동화 생성 이벤트 전파 */
+
+@Service
+@Slf4j(topic = "event")
+@RequiredArgsConstructor
+public class EventService {
+
+    private final KafkaTemplate<String, FairytaleGenerateMessage> kafkaTemplate;
+
+    public void commentMessageCreate(Long userId, FairytaleGenerateReq request) {
+
+        FairytaleGenerateMessage message = FairytaleGenerateMessage.builder()
+                .userId(userId)
+                .background(request.getBackground())
+                .charSpecies(request.getCharSpecie())
+                .morality(request.getMorality())
+                .build();
+
+        log.info("fairytale_generate userId={}, message={}", userId, message);
+
+        kafkaTemplate.send("fairytale_generate", message);
+
+    }
+}
