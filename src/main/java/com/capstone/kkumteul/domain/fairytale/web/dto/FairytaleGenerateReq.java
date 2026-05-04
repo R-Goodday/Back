773b1b4c0252a6aabe1f48ab778e4f2da66c547e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-04 14:37:11 +0900

    Feat: 동화 생성 요청이 들어올 때, 해당 요청을 DTO로 관리할 수 있게끔 객체 선언

---
+++ src/main/java/com/capstone/kkumteul/domain/fairytale/web/dto/FairytaleGenerateReq.java
@@ -0,0 +1,23 @@
+package com.capstone.kkumteul.domain.fairytale.web.dto;
+
+import com.capstone.kkumteul.domain.fairytale.entity.Background;
+import com.capstone.kkumteul.domain.fairytale.entity.CharSpecies;
+import com.capstone.kkumteul.domain.fairytale.entity.Morality;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FairytaleGenerateReq {
+
+    @NotNull(message = "배경은 비어있을 수 없습니다.")
+    private final Background background;
+
+    @NotNull(message = "등장인물 종류는 비어있을 수 없습니다.")
+    private final CharSpecies charSpecie;
+
+    @NotNull(message = "교훈은 비어있을 수 없습니다.")
+    private final Morality morality;
+
+}
