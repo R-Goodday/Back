75d504de52f9e47a2d8d172ac045852b76068ddc
zzuhannn <lunaaeric41@naver.com>
2026-05-10 02:44:37 +0900

    feat: InternalVocabProcessReq.pageNo에 @Min(1) 검증 추가
    페이지 번호는 1-base이므로 0 이하 값을 거절.

--- src/main/java/com/capstone/kkumteul/domain/vocab/web/dto/InternalVocabProcessReq.java
+++ src/main/java/com/capstone/kkumteul/domain/vocab/web/dto/InternalVocabProcessReq.java
@@ -1,5 +1,6 @@
 package com.capstone.kkumteul.domain.vocab.web.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -10,5 +11,6 @@ public class InternalVocabProcessReq {
     private Long fairytaleId;
 
     @NotNull
+    @Min(1)
     private Integer pageNo;
 }
