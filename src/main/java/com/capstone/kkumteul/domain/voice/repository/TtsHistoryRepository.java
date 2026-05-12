7eaac1584f7e85f2acdc2239c43b8b53b7b3a030
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 13:33:51 +0900

    Feat: TtsHistory의 Spring Data JPA interface declaration

---
+++ src/main/java/com/capstone/kkumteul/domain/voice/repository/TtsHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.kkumteul.domain.voice.repository;
+
+import com.capstone.kkumteul.domain.voice.entity.TtsHistory;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TtsHistoryRepository extends CrudRepository<TtsHistory, Long> {
+}
