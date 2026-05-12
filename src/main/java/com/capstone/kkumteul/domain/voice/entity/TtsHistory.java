4b7ffff997ae75ab7aff7ad3bb750fc1c0db94e7
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 13:30:18 +0900

    Feat: 문장 단위로 TTS 파일과 User를 매핑할 수 있는 중간 테이블 선언

---
+++ src/main/java/com/capstone/kkumteul/domain/voice/entity/TtsHistory.java
@@ -0,0 +1,27 @@
+package com.capstone.kkumteul.domain.voice.entity;
+
+import com.capstone.kkumteul.domain.fairytale.entity.Paragraph;
+import com.capstone.kkumteul.domain.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TtsHistory {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paragraph_id")
+    private Paragraph paragraph;
+
+    private String ttsUrl;
+}
