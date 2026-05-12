8dd6458b2b08023a7fe78750c3985aff11fcb9a8
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 11:00:23 +0900

    Feat: 파일 업로드에 실패했을 경우의 예외 코드를 VoiceErrorCode에 작성

--- src/main/java/com/capstone/kkumteul/domain/fairytale/voice/exception/VoiceErrorCode.java
+++ src/main/java/com/capstone/kkumteul/domain/fairytale/voice/exception/VoiceErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum VoiceErrorCode implements BaseResponseCode {
 
-    INVALID_FILE_EXCEPTION("INVALID_FILE_400", 400, "잘못된 파일");
+    INVALID_FILE_EXCEPTION("INVALID_FILE_400", 400, "잘못된 파일"),
+    FILE_UPLOAD_FAIL("FILE_UPLOAD_FAIL_500", 500, "파일을 변환하고, S3에 업로드하는 것에 실패했습니다.");
 
     private final String code;
     private final int httpStatus;
