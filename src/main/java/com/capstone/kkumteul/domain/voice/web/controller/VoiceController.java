dd958d69c3b85f7d453594b1b2050edbd88a7a40
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 14:53:58 +0900

    Feat: TTS 음성 파일이 생성되어 있는지 체크하도록 하는 컨트롤러 메소드, 생성을 요청하는 컨트롤러 메소드 진입점을 각각 선언

--- src/main/java/com/capstone/kkumteul/domain/voice/web/controller/VoiceController.java
+++ src/main/java/com/capstone/kkumteul/domain/voice/web/controller/VoiceController.java
@@ -3,16 +3,14 @@ package com.capstone.kkumteul.domain.voice.web.controller;
 import com.capstone.kkumteul.domain.voice.exception.InvalidFileException;
 import com.capstone.kkumteul.domain.voice.service.VoiceService;
 import com.capstone.kkumteul.domain.user.entity.User;
+import com.capstone.kkumteul.domain.voice.web.dto.TtsFileResponse;
 import com.capstone.kkumteul.global.response.SuccessResponse;
 import com.capstone.kkumteul.global.security.AuthUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -42,4 +40,38 @@ public class VoiceController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(SuccessResponse.created(user.getUserId()));
     }
+
+    @GetMapping("/{paragraphId}")
+    public ResponseEntity<SuccessResponse<?>> getTtsFile(
+            @AuthUser User user,
+            @PathVariable Long paragraphId
+    ) {
+        TtsFileResponse response = voiceService.getTtsFile(user.getId(), paragraphId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.ok(response));
+    }
+
+    @PostMapping("/{paragraphId}")
+    public ResponseEntity<SuccessResponse<?>> postTtsFile(
+            @AuthUser User user,
+            @PathVariable Long paragraphId
+    ) {
+        voiceService.createTtsFile(user.getId(), paragraphId);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(SuccessResponse.accepted());
+    }
+
+    @GetMapping("/{paragraphId}/check")
+    public ResponseEntity<SuccessResponse<?>> hasTtsHistory(
+            @AuthUser User user,
+            @PathVariable Long paragraphId
+    ) {
+        Boolean response = voiceService.hasTtsHistory(user.getId(), paragraphId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.ok(response));
+    }
+
 }
