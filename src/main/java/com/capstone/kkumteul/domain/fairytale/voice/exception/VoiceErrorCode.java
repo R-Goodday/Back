8980a0ae559c72a4cd8d620529101492c9e059c4
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 10:11:39 +0900

    Feat: Voice 도메인에서 사용할 예외 코드를 관리하는 이넘 객체 작성, 잘못된 파일 양식으로 넘어왔을 때 던질 예외 코드를 작성

---
+++ src/main/java/com/capstone/kkumteul/domain/fairytale/voice/exception/VoiceErrorCode.java
@@ -0,0 +1,16 @@
+package com.capstone.kkumteul.domain.fairytale.voice.exception;
+
+import com.capstone.kkumteul.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum VoiceErrorCode implements BaseResponseCode {
+
+    INVALID_FILE_EXCEPTION("INVALID_FILE_400", 400, "잘못된 파일")
+
+    private final int code;
+    private final int httpStatus;
+    private final String message;
+}
