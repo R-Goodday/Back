03122b3fb04d93db41cc548619ad2abc587f7bba
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 11:24:42 +0900

    Feat: 음성 녹음 파일을 S3에 저장하고, Entity를 빌드 한 후 Kafka에 메시지를 전송하는 EventService를 호출하는 로직을 VoiceService의 구현체 내에 하나의 메소드로 작성

---
+++ src/main/java/com/capstone/kkumteul/domain/fairytale/voice/service/VoiceServiceImpl.java
@@ -0,0 +1,57 @@
+package com.capstone.kkumteul.domain.fairytale.voice.service;
+
+import com.capstone.kkumteul.domain.fairytale.voice.entity.VoiceModel;
+import com.capstone.kkumteul.domain.fairytale.voice.exception.FileUploadFailException;
+import com.capstone.kkumteul.domain.fairytale.voice.repository.VoiceModelRepository;
+import com.capstone.kkumteul.domain.fairytale.voice.web.dto.TtsModelingRequest;
+import com.capstone.kkumteul.domain.kafka.service.EventService;
+import com.capstone.kkumteul.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class VoiceServiceImpl implements VoiceService {
+
+    private final S3Uploader s3Uploader;
+    private final VoiceModelRepository voiceModelRepository;
+    private final EventService eventService;
+
+    @Override
+    @Transactional
+    public TtsModelingRequest saveMp3(MultipartFile wavFile, User user) {
+
+        String uploadedUrl;
+
+        try {
+            uploadedUrl = s3Uploader.upload(wavFile, user);
+        } catch (IOException e) {
+            log.error("VoiceService error occurred. userId = {}, filename = {}", user.getId(), wavFile.getOriginalFilename());
+            throw new FileUploadFailException();
+        }
+
+        VoiceModel saved = VoiceModel.builder()
+                .user(user)
+                .originFilename(wavFile.getOriginalFilename())
+                .build();
+
+        voiceModelRepository.save(saved);
+
+        TtsModelingRequest result = sendKafkaMessage(user.getId(), uploadedUrl);
+
+        return result;
+    }
+
+    private TtsModelingRequest sendKafkaMessage(Long userId, String uploadedUrl) {
+        TtsModelingRequest requestBody = new TtsModelingRequest(userId, uploadedUrl);
+        eventService.sendTtsModelingRequest(requestBody);
+
+        return requestBody;
+    }
+}
