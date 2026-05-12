dcda66f680faf5c9cf79ad76078e453dd081277e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-05-12 11:20:50 +0900

    Feat: userId와 TTS 모델 이름을 매핑한 엔터티를 초기화하기 위한 Repository를 Spring Data JPA 방식 인터페이스로 작성

---
+++ src/main/java/com/capstone/kkumteul/domain/fairytale/voice/repository/VoiceModelRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.kkumteul.domain.fairytale.voice.repository;
+
+import com.capstone.kkumteul.domain.fairytale.voice.entity.VoiceModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoiceModelRepository extends JpaRepository<VoiceModel, Long> {
+}
