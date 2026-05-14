d759da90aff06d15279b9ea17860ca39a78c79a6
zzuhannn <lunaaeric41@naver.com>
2026-05-14 20:46:24 +0900

    hotfix: CORS 설정 추가

--- src/main/java/com/capstone/kkumteul/global/config/SecurityConfig.java
+++ src/main/java/com/capstone/kkumteul/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -60,6 +61,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // CORS 활성화 (WebConfig.addCorsMappings 사용)
+                .cors(Customizer.withDefaults())
+
                 // JWT 기반 API 서버에서는 stateless를 지향하기 때문에, 세션 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
 
